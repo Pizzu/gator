7bc9f50e064964abd800e1e30f7d84788010ab3b
Luca Lo Forte <pizzulao@gmail.com>
2025-03-16 20:50:35 +0100

    feat: add postgres db and generate client with sqlc

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
 	"github.com/Pizzu/gator/internal/config"
+	"github.com/Pizzu/gator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
 	cfg *config.Config
+	db  *database.Queries
 }
 
 func main() {
@@ -17,8 +21,19 @@ func main() {
 		log.Fatalf("error reading config: %v", err)
 	}
 
+	db, err := openDB(cfg.DbURL)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	defer closeDB(db)
+
+	dbQueries := database.New(db)
+
 	programState := &state{
 		cfg: &cfg,
+		db:  dbQueries,
 	}
 
 	cmds := commands{
@@ -37,5 +52,23 @@ func main() {
 	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func closeDB(db *sql.DB) {
+	err := db.Close()
+	if err != nil {
+		log.Fatal("Error while closing DB connection")
 	}
 }
